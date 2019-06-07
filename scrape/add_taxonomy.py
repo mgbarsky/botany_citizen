61e45dcf32596c90854d0c358218611abffcfb31
askdfjlas <kaihengd@gmail.com>
2019-06-06 22:21:12 -0400

    add taxonomy

---
+++ scrape/add_taxonomy.py
@@ -0,0 +1,51 @@
+# Add full taxonomy to flower_data.tsv
+import json
+
+plant_json = None
+taxonomy_map = {}
+
+
+def recur(element, path):
+    if isinstance(element, list):
+        taxonomy_map[path[0]] = path
+    else:
+        for key in element:
+            recur(element[key], [key] + path)
+
+
+def generate_map(filename):
+    global plant_json
+    input_f = open(filename)
+    plant_json = json.load(input_f)
+    input_f.close()
+
+    recur(plant_json, [])
+
+
+def taxonomy_table(input_name, output_name):
+    input_f = open(input_name)
+    output_f = open(output_name, 'w')
+
+    header = True
+    for line in input_f:
+        if header:
+            header = False
+            output_f.write(line)
+            continue
+        arr = line.rstrip().split('\t')
+        species = arr[-1]
+        genus = species.split(' ')[0]
+        if genus in taxonomy_map:
+            path = taxonomy_map[genus]
+            arr += path
+        else:
+            print("Path for {} not found!".format(genus))
+            continue
+        output_f.write('\t'.join(arr) + '\n')
+    input_f.close()
+    output_f.close()
+
+
+if __name__ == "__main__":
+    generate_map("taxonomy3.json")
+    taxonomy_table("flower_data.tsv", "d_flower_data.tsv")
